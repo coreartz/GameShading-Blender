b2598dd602f76cfe37afb199b845d3e5e28be1d7
Janik "Core" M <Janik.m@coreartz.de>
2023-07-30 22:37:03 +0200

    unk_semantic_0x34 check moved
    unk_semantic_0x34 get checked at start. Shaders get created after decieded between Skin and normal shader

--- COD/MW2 2022/AutoShading.py
+++ COD/MW2 2022/AutoShading.py
@@ -15,17 +15,17 @@ import math
 
 # CHANGE THIS 2 PATHS TO THE SHADER.BLEND AND SEMODEL FILE YOU JUST EXPORTED ON YOUR PC
 shader_blend = R"F:\Coding\GameShading-Blender\COD\MW2 2022\MW2_Shader.blend"
-Path = R"E:\Game Porting\COD MW2 2022\CDL\Faze\Away\body_mp_cdl_male_iw9_faze_away\body_mp_cdl_male_iw9_faze_away_LOD0.semodel"
+Path = R"E:\Game Porting\COD MW2 2022\Cat 1\head_mp_cat_iw9_1_1\head_mp_cat_iw9_1_1_LOD0.semodel"
 
 #IMAGE FILEFORMAT
 img_format = ".png"
 
 # GLOBAL IMAGE SETTINGS
-UseGlobalImages = True
+UseGlobalImages = False
 GlobalImagePath = R"E:\Game Porting\COD MW2 2022\CDL\Faze\Away\body_mp_cdl_male_iw9_faze_away\_images"
 
 # CDL SKIN SETTINGS
-IsCDLskin = True
+IsCDLskin = False
 # [Red,Green,Blue,Alpha] 
 # Color1 = Mask Red, Color2 = Mask Green, Color3 = Mask Blue
 AlbedoTint = [0.1,0.1,0.1,1]
@@ -84,7 +84,10 @@ for dirpath, dirnames, filenames in os.walk(Folder):
 
                 lines = list(map(lambda s: s.strip(), lines))
                 lines.remove("semantic,image_name")
-                
+                for i in range(len(lines)):
+                    if lines[i].split(",")[0] == "unk_semantic_0x34":
+                        shader.node_tree = bpy.data.node_groups.get("COD Skin") 
+
                 for i in range(len(lines)):
                     if lines[i].split(",")[0] == "unk_semantic_0x0":
                         if lines[i].split(",")[1] == "ximage_3c29eeff15212c37":
@@ -136,116 +139,120 @@ for dirpath, dirnames, filenames in os.walk(Folder):
                                 link(tex_image_node.outputs["Alpha"], shader.inputs["NRA Alpha"])
                                 tex_image_node.image.colorspace_settings.name = "Non-Color"
                     elif lines[i].split(",")[0] == "unk_semantic_0x8":
-                        if lines[i].split(",")[1] == "ximage_3c29eeff15212c37":
-                            shader.inputs["Emission"].default_value = [1,1,1,1]
-                        elif lines[i].split(",")[1] == "ximage_4a882744bc523875":
-                            shader.inputs["Emission"].default_value = [0,0,0,1]
-                        else:
-                            if os.path.isfile(img_folder + "\\" + lines[i].split(",")[1] + img_format):
-                                Emission = get_image(lines[i].split(",")[1], img_folder + "\\" + lines[i].split(",")[1] + img_format)
-                                tex_image_node: bpy.types.Node
-                                tex_image_node = mat.node_tree.nodes.new('ShaderNodeTexImage')
-                                tex_image_node.location = (-500,0)
-                                tex_image_node.image = Emission
-                                tex_image_node.label = "Emission"
-                                tex_image_node.image.alpha_mode = "CHANNEL_PACKED"
-                                link(tex_image_node.outputs["Color"], shader.inputs["Emission"])
+                        if "Emission" in shader.inputs:
+                            if lines[i].split(",")[1] == "ximage_3c29eeff15212c37":
+                                shader.inputs["Emission"].default_value = [1,1,1,1]
+                            elif lines[i].split(",")[1] == "ximage_4a882744bc523875":
+                                shader.inputs["Emission"].default_value = [0,0,0,1]
+                            else:
+                                if os.path.isfile(img_folder + "\\" + lines[i].split(",")[1] + img_format):
+                                    Emission = get_image(lines[i].split(",")[1], img_folder + "\\" + lines[i].split(",")[1] + img_format)
+                                    tex_image_node: bpy.types.Node
+                                    tex_image_node = mat.node_tree.nodes.new('ShaderNodeTexImage')
+                                    tex_image_node.location = (-500,0)
+                                    tex_image_node.image = Emission
+                                    tex_image_node.label = "Emission"
+                                    tex_image_node.image.alpha_mode = "CHANNEL_PACKED"
+                                    link(tex_image_node.outputs["Color"], shader.inputs["Emission"])
                     elif lines[i].split(",")[0] == "unk_semantic_0xC":
-                        if lines[i].split(",")[1] == "ximage_3c29eeff15212c37":
-                            shader.inputs["Alpha"].default_value = 1
-                        elif lines[i].split(",")[1] == "ximage_4a882744bc523875":
-                            shader.inputs["Alpha"].default_value = 0
-                            mat.blend_method = "HASHED"
-                        else:
-                            if os.path.isfile(img_folder + "\\" + lines[i].split(",")[1] + img_format):
+                        if "Alpha" in shader.inputs:
+                            if lines[i].split(",")[1] == "ximage_3c29eeff15212c37":
+                                shader.inputs["Alpha"].default_value = 1
+                            elif lines[i].split(",")[1] == "ximage_4a882744bc523875":
+                                shader.inputs["Alpha"].default_value = 0
                                 mat.blend_method = "HASHED"
-                                Alpha = get_image(lines[i].split(",")[1], img_folder + "\\" + lines[i].split(",")[1] + img_format)
-                                tex_image_node: bpy.types.Node
-                                tex_image_node = mat.node_tree.nodes.new('ShaderNodeTexImage')
-                                tex_image_node.location = (-500,-300)
-                                tex_image_node.image = Alpha
-                                tex_image_node.label = "Alpha"
-                                tex_image_node.image.alpha_mode = "CHANNEL_PACKED"
-                                link(tex_image_node.outputs["Color"], shader.inputs["Alpha"])
-                                tex_image_node.image.colorspace_settings.name = "Non-Color"
+                            else:
+                                if os.path.isfile(img_folder + "\\" + lines[i].split(",")[1] + img_format):
+                                    mat.blend_method = "HASHED"
+                                    Alpha = get_image(lines[i].split(",")[1], img_folder + "\\" + lines[i].split(",")[1] + img_format)
+                                    tex_image_node: bpy.types.Node
+                                    tex_image_node = mat.node_tree.nodes.new('ShaderNodeTexImage')
+                                    tex_image_node.location = (-500,-300)
+                                    tex_image_node.image = Alpha
+                                    tex_image_node.label = "Alpha"
+                                    tex_image_node.image.alpha_mode = "CHANNEL_PACKED"
+                                    link(tex_image_node.outputs["Color"], shader.inputs["Alpha"])
+                                    tex_image_node.image.colorspace_settings.name = "Non-Color"
                     elif lines[i].split(",")[0] == "unk_semantic_0x22":
-                        if lines[i].split(",")[1] == "ximage_3c29eeff15212c37":
-                            shader.inputs["Transparency"].default_value = 1
-                            shader.inputs["Use Transparency"].default_value = 1
-                            mat.blend_method = "BLEND"
-                        elif lines[i].split(",")[1] == "ximage_4a882744bc523875":
-                            shader.inputs["Transparency"].default_value = 0
-                        else:
-                            if os.path.isfile(img_folder + "\\" + lines[i].split(",")[1] + img_format):
-                                Transparency = get_image(lines[i].split(",")[1], img_folder + "\\" + lines[i].split(",")[1] + img_format)
-                                tex_image_node: bpy.types.Node
-                                tex_image_node = mat.node_tree.nodes.new('ShaderNodeTexImage')
-                                tex_image_node.location = (-500,-600)
-                                tex_image_node.image = Transparency
-                                tex_image_node.label = "Transparency"
-                                tex_image_node.image.alpha_mode = "CHANNEL_PACKED"
+                        if "Transparency" in shader.inputs:
+                            if lines[i].split(",")[1] == "ximage_3c29eeff15212c37":
+                                shader.inputs["Transparency"].default_value = 1
                                 shader.inputs["Use Transparency"].default_value = 1
                                 mat.blend_method = "BLEND"
-                                link(tex_image_node.outputs["Color"], shader.inputs["Transparency"])
+                            elif lines[i].split(",")[1] == "ximage_4a882744bc523875":
+                                shader.inputs["Transparency"].default_value = 0
+                            else:
+                                if os.path.isfile(img_folder + "\\" + lines[i].split(",")[1] + img_format):
+                                    Transparency = get_image(lines[i].split(",")[1], img_folder + "\\" + lines[i].split(",")[1] + img_format)
+                                    tex_image_node: bpy.types.Node
+                                    tex_image_node = mat.node_tree.nodes.new('ShaderNodeTexImage')
+                                    tex_image_node.location = (-500,-600)
+                                    tex_image_node.image = Transparency
+                                    tex_image_node.label = "Transparency"
+                                    tex_image_node.image.alpha_mode = "CHANNEL_PACKED"
+                                    shader.inputs["Use Transparency"].default_value = 1
+                                    mat.blend_method = "BLEND"
+                                    link(tex_image_node.outputs["Color"], shader.inputs["Transparency"])
                     elif lines[i].split(",")[0] == "unk_semantic_0x26":
-                        if lines[i].split(",")[1] == "ximage_3c29eeff15212c37":
-                            shader.inputs["Specular"].default_value = 1
-                        elif lines[i].split(",")[1] == "ximage_4a882744bc523875":
-                            shader.inputs["Specular"].default_value = 0
-                            mat.blend_method = "HASHED"
-                        else:
-                            if os.path.isfile(img_folder + "\\" + lines[i].split(",")[1] + img_format):
-                                Spec_Mask = get_image(lines[i].split(",")[1], img_folder + "\\" + lines[i].split(",")[1] + img_format)
-                                tex_image_node: bpy.types.Node
-                                tex_image_node = mat.node_tree.nodes.new('ShaderNodeTexImage')
-                                tex_image_node.location = (-500,-900)
-                                tex_image_node.image = Spec_Mask
-                                tex_image_node.image.alpha_mode = "CHANNEL_PACKED"
-                                tex_image_node.image.colorspace_settings.name = "Non-Color"
-                                link(tex_image_node.outputs["Color"], shader.inputs["Specular"])
-                                tex_image_node.label = "Specular_Mask"
-                                pixel_float = Spec_Mask.size[0] * Spec_Mask.size[1] * 4
-                                test_num = (Spec_Mask.size[0] + Spec_Mask.size[1]) / 2 * 32
-                                am =int(pixel_float/4/test_num)
-                                r_px = 0
-                                SSS = False
-                                for i in range(am):
-                                    R = math.floor(Spec_Mask.pixels[r_px] *100)
-                                    G = math.floor(Spec_Mask.pixels[r_px + 1] *100)
-                                    B = math.floor(Spec_Mask.pixels[r_px + 2] *100)
-                                    if B != 0:
-                                        SSS = True
-                                    if R != G:
-                                        mat.node_tree.links.remove(shader.inputs["Specular"].links[0])
-                                        if SSS:
-                                            link(tex_image_node.outputs["Color"], shader.inputs["SSS"])
-                                            link(tex_image_node.outputs["Alpha"], shader.inputs["SSS Alpha"])
-                                            tex_image_node.label = "SSS"
-                                            tex_image_node.image.colorspace_settings.name = "sRGB"
-                                        else:
-                                            tex_image_node.label = "0x26"
-                                        break
-                                    elif B != G:
-                                        mat.node_tree.links.remove(shader.inputs["Specular"].links[0])
-                                        if SSS:
-                                            link(tex_image_node.outputs["Color"], shader.inputs["SSS"])
-                                            link(tex_image_node.outputs["Alpha"], shader.inputs["SSS Alpha"])
-                                            tex_image_node.label = "SSS"
-                                            tex_image_node.image.colorspace_settings.name = "sRGB"
-                                        else:
-                                            tex_image_node.label = "0x26"
-                                        break
-                                    elif R != B:
-                                        mat.node_tree.links.remove(shader.inputs["Specular"].links[0])
-                                        if SSS:
-                                            link(tex_image_node.outputs["Color"], shader.inputs["SSS"])
-                                            link(tex_image_node.outputs["Alpha"], shader.inputs["SSS Alpha"])
-                                            tex_image_node.label = "SSS"
-                                            tex_image_node.image.colorspace_settings.name = "sRGB"
-                                        else:
-                                            tex_image_node.label = "0x26"
-                                        break 
-                                    r_px = r_px + int(test_num*4)
+                        if "Specular" in shader.inputs:
+                            if lines[i].split(",")[1] == "ximage_3c29eeff15212c37":
+                                shader.inputs["Specular"].default_value = 1
+                            elif lines[i].split(",")[1] == "ximage_4a882744bc523875":
+                                shader.inputs["Specular"].default_value = 0
+                                mat.blend_method = "HASHED"
+                            else:
+                                if os.path.isfile(img_folder + "\\" + lines[i].split(",")[1] + img_format):
+                                    Spec_Mask = get_image(lines[i].split(",")[1], img_folder + "\\" + lines[i].split(",")[1] + img_format)
+                                    tex_image_node: bpy.types.Node
+                                    tex_image_node = mat.node_tree.nodes.new('ShaderNodeTexImage')
+                                    tex_image_node.location = (-500,-900)
+                                    tex_image_node.image = Spec_Mask
+                                    tex_image_node.image.alpha_mode = "CHANNEL_PACKED"
+                                    tex_image_node.image.colorspace_settings.name = "Non-Color"
+                                    link(tex_image_node.outputs["Color"], shader.inputs["Specular"])
+                                    tex_image_node.label = "Specular_Mask"
+                                    pixel_float = Spec_Mask.size[0] * Spec_Mask.size[1] * 4
+                                    test_num = (Spec_Mask.size[0] + Spec_Mask.size[1]) / 2 * 32
+                                    am =int(pixel_float/4/test_num)
+                                    r_px = 0
+                                    SSS = False
+                                    for i in range(am):
+                                        R = math.floor(Spec_Mask.pixels[r_px] *100)
+                                        G = math.floor(Spec_Mask.pixels[r_px + 1] *100)
+                                        B = math.floor(Spec_Mask.pixels[r_px + 2] *100)
+                                        if B != 0:
+                                            SSS = True
+                                        if R != G:
+                                            mat.node_tree.links.remove(shader.inputs["Specular"].links[0])
+                                            if SSS:
+                                                link(tex_image_node.outputs["Color"], shader.inputs["SSS"])
+                                                link(tex_image_node.outputs["Alpha"], shader.inputs["SSS Alpha"])
+                                                tex_image_node.label = "SSS"
+                                                tex_image_node.image.colorspace_settings.name = "sRGB"
+                                            else:
+                                                tex_image_node.label = "0x26"
+                                            break
+                                        elif B != G:
+                                            mat.node_tree.links.remove(shader.inputs["Specular"].links[0])
+                                            if SSS:
+                                                link(tex_image_node.outputs["Color"], shader.inputs["SSS"])
+                                                link(tex_image_node.outputs["Alpha"], shader.inputs["SSS Alpha"])
+                                                tex_image_node.label = "SSS"
+                                                tex_image_node.image.colorspace_settings.name = "sRGB"
+                                            else:
+                                                tex_image_node.label = "0x26"
+                                            break
+                                        elif R != B:
+                                            mat.node_tree.links.remove(shader.inputs["Specular"].links[0])
+                                            if SSS:
+                                                link(tex_image_node.outputs["Color"], shader.inputs["SSS"])
+                                                link(tex_image_node.outputs["Alpha"], shader.inputs["SSS Alpha"])
+                                                tex_image_node.label = "SSS"
+                                                tex_image_node.image.colorspace_settings.name = "sRGB"
+                                            else:
+                                                tex_image_node.label = "0x26"
+                                            break 
+                                        r_px = r_px + int(test_num*4)
                     elif lines[i].split(",")[0] == "unk_semantic_0x32":
                         if IsCDLskin:
                             if lines[i].split(",")[1] == "ximage_3c29eeff15212c37":
@@ -281,5 +288,3 @@ for dirpath, dirnames, filenames in os.walk(Folder):
                                         if B == 0:
                                             shader.inputs["Activate Color 3"].default_value = 1
                                         r_px = r_px + int(test_num*4)
-                    elif lines[i].split(",")[0] == "unk_semantic_0x34":
-                        shader.node_tree = bpy.data.node_groups.get("COD Skin") 
